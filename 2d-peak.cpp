c5729b6400de575812778871ea75f3519c31d567
heronhoga <nozzeravixer@gmail.com>
2025-03-16 11:58:59 +0700

    fix:fix binary search

--- 2d-peak.cpp
+++ 2d-peak.cpp
@@ -3,21 +3,30 @@
 
 using namespace std;
 
-int findMaxIndex(vector<vector<int>> &matrix, int col) {
-    int maxIndex = 0;
-    for (int i = 1; i < matrix.size(); i++) {
-        if (matrix[i][col] > matrix[maxIndex][col]) {
-            maxIndex = i;
-        }
+int findMaxIndex(vector<vector<int>> &matrix, int col, int top, int bottom) {
+    if (top == bottom) return top;
+
+    int midRow = (top + bottom) / 2; 
+
+    int current = matrix[midRow][col];
+    int above = (midRow > 0) ? matrix[midRow - 1][col] : -1;
+    int below = (midRow < matrix.size() - 1) ? matrix[midRow + 1][col] : -1;
+
+    if (current >= above && current >= below) {
+        return midRow;
     }
-    return maxIndex;
+
+    if (midRow > 0 && above > current) {
+        return findMaxIndex(matrix, col, top, midRow - 1);
+    }
+    return findMaxIndex(matrix, col, midRow + 1, bottom);
 }
 
 int find2DPeak(vector<vector<int>> &matrix, int leftCol, int rightCol) {
     if (leftCol > rightCol) return -1;
 
     int midCol = (leftCol + rightCol) / 2;
-    int maxRow = findMaxIndex(matrix, midCol);
+    int maxRow = findMaxIndex(matrix, midCol, 0, matrix.size() - 1);
 
     int current = matrix[maxRow][midCol];
     int left = (midCol > 0) ? matrix[maxRow][midCol - 1] : -1;
@@ -29,9 +38,9 @@ int find2DPeak(vector<vector<int>> &matrix, int leftCol, int rightCol) {
 
     if (left > current) {
         return find2DPeak(matrix, leftCol, midCol - 1);
-    } else {
-        return find2DPeak(matrix, midCol + 1, rightCol); 
     }
+
+    return find2DPeak(matrix, midCol + 1, rightCol);
 }
 
 int find2DPeak(vector<vector<int>> &matrix) {
@@ -42,7 +51,7 @@ int main() {
     vector<vector<int>> matrix = {
         {10, 20, 15},
         {21, 30, 14},
-        {7, 16, 32}
+        {7, 40, 32}
     };
 
     cout << "2D Peak: " << find2DPeak(matrix) << endl;
